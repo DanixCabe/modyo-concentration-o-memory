63a2451db25ebe2d97f856fde98b48e887221109
Daniel Gizzi <mut_dgizzi@externos.entel.cl>
2023-09-15 09:32:25 -0300

    update Offcanvas

--- src/components/Offcanvas.jsx
+++ src/components/Offcanvas.jsx
@@ -1,10 +1,11 @@
+import { Popover } from 'flowbite';
 import {GiCardJoker ,GiRollingDices} from 'react-icons/gi'
 
 export const Offcanvas = ({username}) => {
     return (
         <>
     
-            <div id="drawer-profile" className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full w-64 bg-gray-800" tabIndex="-1" aria-labelledby="drawer-profile-label">
+            <div id="drawer-profile" className="fixed top-0 left-0 z-40 h-screen p-4 transition-transform -translate-x-full overflow-hidden bg-gray-800" tabIndex="-1" aria-labelledby="drawer-profile-label">
                 <h5 id="drawer-profile-label" className="text-base font-semibold text-gray-400">{username}</h5>
                 <button type="button" data-drawer-hide="drawer-profile" aria-controls="drawer-profile" className="text-gray-400 bg-transparent  rounded-lg text-sm w-8 h-8 absolute top-2.5 right-2.5 inline-flex items-center justify-center hover:bg-gray-600 hover:text-white" >
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -21,10 +22,19 @@ export const Offcanvas = ({username}) => {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2  rounded-lg text-white hover:bg-gray-700 group">
+                            <a href="#"  data-popover-target="popover-bottom" data-popover-placement="bottom" className="flex items-center p-2  rounded-lg text-white hover:bg-gray-700 group">
                                 <GiRollingDices className='text-3xl'/>
                                 <span className="ml-3 text-sm">Coming soon...</span>
                             </a>
+                            <div data-popover id="popover-bottom" role="tooltip" class="absolute z-50 invisible inline-block w-[80%] text-sm transition-opacity rounded-lg shadow-sm opacity-0 text-gray-200 border-gray-100 bg-slate-900">
+                                <div class="px-3 py-2 border-b  rounded-t-lg border-gray-600 bg-gray-700">
+                                    <h3 class="font-semibol text-white">Dices Game</h3>
+                                </div>
+                                <div class="px-3 py-2">
+                                    <p>Our developers work in this game.</p>
+                                </div>
+                                <div data-popper-arrow></div>
+                            </div>
                         </li>
                     </ul>
                 </div>
